4e79d642dd36187f9f3948a5cd9564f8c68e4fb9
khalilbaidouri <khalilbaidouri97@gmail.com>
2025-05-05 23:28:50 +0000

    Create GroupeRepesitory.java

---
+++ src/main/java/com/example/javaprojet/repo/package com.example.javaprojet.repo;  import org.example.gestionprojet.entities.Groupe; import org.springframework.data.jpa.repository.JpaRepository;  /**  @author $ {USERS}  **/GroupeRepesitory.java
@@ -0,0 +1,7 @@
+package com.example.javaprojet.repo;
+
+import org.example.gestionprojet.entities.Groupe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupeRepesitory extends JpaRepository<Groupe, Long> {
+}
