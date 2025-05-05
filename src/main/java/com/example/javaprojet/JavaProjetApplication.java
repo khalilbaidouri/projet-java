c45779aaca59204911abfc7e06c832eb43488d81
badr eddine el abbady <badreddinne000@gmail.com>
2025-05-05 15:58:40 +0100

    Initial commit

---
+++ src/main/java/com/example/javaprojet/JavaProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjetApplication.class, args);
+    }
+
+}
